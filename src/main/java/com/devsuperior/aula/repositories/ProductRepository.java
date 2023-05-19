ddef9ae63510778255ca7cb0fe088abaf0967c88
Elves Aguiar <elvesmhs@hotmail.com>
2023-05-19 15:42:06 -0300

    feat: save Many to Many entity by DTO

--- src/main/java/com/devsuperior/aula/repositories/ProductRepository.java
+++ src/main/java/com/devsuperior/aula/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.devsuperior.aula.repositories;
 
 import com.devsuperior.aula.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
